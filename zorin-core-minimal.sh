d88cbfe2220bbd980d9054616ea990656ba65796
ianaraujo <ianaraujo15@gmail.com>
2022-01-30 14:22:56 -0300

    v1

---
+++ zorin-core-minimal.sh
@@ -0,0 +1,23 @@
+sudo apt-get update
+
+sudo apt-get remove brasero -y
+sudo apt-get remove cheese -y
+sudo apt-get remove aisleriot -y
+sudo apt-get remove evolution -y
+sudo apt-get remove pitivi -y
+sudo apt-get remove quadrapassel -y
+sudo apt-get remove rhythmbox -y
+sudo apt-get remove remmina -y
+sudo apt-get remove gnome-mahjongg -y
+sudo apt-get remove gnome-mines -y
+sudo apt-get remove gnome-photos -y
+sudo apt-get remove gnome-sudoku -y
+sudo apt-get remove gnome-tour -y
+sudo apt-get remove gnome-todo -y
+# sudo apt-get remove libreoffice -y
+# sudo apt-get remove libreoffice-calc -y
+# sudo apt-get remove libreoffice-draw -y
+# sudo apt-get remove libreoffice-impress -y
+# sudo apt-get remove libreoffice-writer -y
+
+sudo apt-get autoremove
